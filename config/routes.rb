9d22886fefad59a5c8b67b17f0043b4ca6cfbf9a
baohua <113983633@qq.com>
2017-03-17 13:40:52 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :topics do
-  member do
-    post 'upvote'
-  end
-end
-root 'topics#index'
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
